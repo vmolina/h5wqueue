07fca7b0fa5c6f8b4a6bad49fcc8fc6c6b20a407
victor <vbellonbcn@gmail.com>
2014-03-13 18:04:57 +0100

    Created setup file and project structure.

---
+++ h5wqueue/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="h5wqueue",
+      version="0.1",
+      description="Simple utilities for write hdf5 files using workers",
+      url='http://github.com/vmolina/h5wqueue',
+      author='Victor Bellon Molina',
+      author_email='vbellon@gmail.com',
+      license='GPLv3',
+      packages=['h5wqueue'],
+      install_requires=[
+          'pytables', 'rq'
+      ],
+)
